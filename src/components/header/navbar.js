127a6cab95359823f8d6ca74ac2b689c8c5380f3
fanani <fananiapps@gmail.com>
2021-08-01 18:04:43 +0700

    Fix hamburger menu

--- src/components/header/navbar.js
+++ src/components/header/navbar.js
@@ -4,7 +4,7 @@ import { MenuIcon, XIcon } from '@heroicons/react/solid'
 
 export default function navbar() {
     return (
-        <nav className="bg-white bg-opacity-60 py-4 px-4 md:px-14 lg:px-20">
+        <nav className="bg-white bg-opacity-60 py-4 px-8 md:px-14 lg:px-20">
             <div className="flex justify-between">
                 <div className="my-auto flex">
                     <h1 className="font-semibold font-nunito text-3xl leading-none text-green-400">Beauty </h1>
@@ -46,23 +46,22 @@ export default function navbar() {
                         <Popover.Panel
                             focus
                             static
-                            className="absolute bg-gray-900 bg-opacity-20 top-0 inset-x-0 transition transform origin-top-right md:hidden lg:hidden xl:hidden"
+                            className="absolute bg-gray-900 bg-opacity-20 top-0 bottom-0 inset-x-0 transition transform origin-top-right md:hidden lg:hidden xl:hidden"
                         >
-                            <div className="rounded-lg relative py-6 px-6 w-8/12 md:w-1/2 h-screen shadow-md bg-white overflow-hidden">
-                                <div className="mt-3 flex items-center justify-between">
-                                    <div className="flex mb-8">
-                                        <h1 className="font-semibold font-nunito text-2xl text-green-500">Beauty </h1>
-                                        <h1 className="font-semibold font-nunito text-2xl">Gems</h1>
-                                    </div>
-                                    <div className="-mr-2">
-                                    <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none">
-                                        <span className="sr-only">Close main menu</span>
-                                        <XIcon className="h-6 w-6 text-green-500" aria-hidden="true" />
-                                    </Popover.Button>
+                            <div className="fixed top-0 left-0 bottom-0 rounded-lg py-6 px-6 w-5/6 md:w-1/2 h-screen shadow-md bg-white overflow-hidden">
+                                <div className="flex items-center justify-between px-6">
+                                    <h1 className="font-semibold font-nunito text-2xl text-green-500">
+                                        Beauty<span className="text-black">Gems</span>
+                                    </h1>
+                                    <div className="-mr-2 flex items-center">
+                                        <Popover.Button className="bg-white rounded-md inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none">
+                                            <span className="sr-only">Close main menu</span>
+                                            <XIcon className="h-6 w-6 text-green-500" aria-hidden="true" />
+                                        </Popover.Button>
                                     </div>
                                 </div>
-                                <div className="mt-10 mb-4">
-                                    <ul className="space-y-10">
+                                <div className="mt-10 mb-4 px-6">
+                                    <ul className="space-y-5">
                                         <li className="my-auto font-nunito">Home</li>
                                         <li className="my-auto font-nunito">Browse</li>
                                         <li className="my-auto font-nunito">Source More</li>
@@ -71,8 +70,8 @@ export default function navbar() {
                                         <li className="my-auto font-nunito">Register</li>
                                     </ul>
                                 </div>
-                                <div className="mt-auto">
-                                    <div className="pt-6">
+                                <div className="absolute bottom-0 px-6 w-full">
+                                    <div className="pt-6 w-full">
                                         <a className="font-nunito py-3 px-4 mb-3 w-full flex justify-center bg-green-500 text-white rounded-md">
                                             Post Request
                                         </a>
---
+++ src/components/homepage/section1.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { SearchIcon } from '@heroicons/react/solid'
+
+export default function section1() {
+    return (
+        <div className="bg-gray-200">
+            <div className="grid grid-cols-1 md:grid-cols-2">
+                <div className='py-8 md:py-14 mx-8 lg:mx-20'>
+                    <div className="text-sm lg:text-lg my-4">
+                        <h1 className="font-nunito text-gray-500">Beauty Gems</h1>
+                    </div>
+                    <div className="uppercase text-6xl lg:text-7xl 2xl:text-8xl font-bold font-openSans my-4">
+                        <span>
+                            start <span className="text-green-500">selling</span> online for <span className="text-green-500">free</span>
+                        </span>
+                    </div>
+                    <div className="text-sm lg:text-lg my-4">
+                        <p className="text-gray-500 font-nunito">
+                            Beauty Gems allow you to choose the best product to sell from thousand dropshipping suppliers all over the world.
+                        </p>
+                    </div>
+                    <div className="flex">
+                        <input
+                            className="w-10/12 md:w-5/12 lg:w-9/12 xl:w-7/12 2xl:w-5/12 h-10 placeholder-gray-500 px-5"
+                            placeholder="Enter Keywords"
+                        />
+                        <button className="p-4 bg-green-500">
+                            <SearchIcon className="h-3 w-3 text-white"/>
+                        </button>
+                    </div>
+                </div>
+                <div className="hidden md:block">
+                    <img className="w-4/6 h-4/6 my-40 mx-auto object-cover"
+                        src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fdeifpxeochufn.cloudfront.net%2Fwp-content%2Fuploads%2F2017%2F06%2Fecommerce-2.png&f=1&nofb=1"
+                    />
+                </div>
+            </div>
+        </div>
+    )
+}
